b5b5470a9ac6f873134a078fb450f72c02a3e0d7
Hassuna2003 <jmmoujirh01@iesalbarregas.es>
2024-11-30 14:16:01 +0100

    Update balanceador.sh

--- Scripts_aprov/balanceador.sh
+++ Scripts_aprov/balanceador.sh
@@ -1,1 +1,17 @@
+# Actualizamos repositorios
+sudo apt update -y
+
+# Instalamos apache, que lo usaremos como balanceador de carga
+sudo apt install apache2 -y
+
+# Configurar el servidor web Apache como proxy inverso. Tenemos que activar estos módulos como mínimo
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_balancer
+
+# Este método de balanceo de carga consiste en distribuir las peticiones entre los servidores de forma secuencial, de forma que cada vez que llegue una nueva petición se envía al siguiente servidor de la lista de servidores configurados en el servidor Apache
+sudo a2enmod lbmethod_byrequests
+
+# Reiniciamos apache para aplicar cambios
+sudo systemctl restart apache2
 
