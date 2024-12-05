d74ccd6d981f22b38889ee1e31aa08dd7bf74133
Hassuna2003 <jmmoujirh01@iesalbarregas.es>
2024-12-05 16:22:19 +0100

    Update base_datos.sh

--- Scripts_aprov/base_datos.sh
+++ Scripts_aprov/base_datos.sh
@@ -1,1 +1,14 @@
 
+sudo apt update -y
+
+sudo apt install mysql-server -y
+sudo apt install -y phpmyadmin
+sudo sed -i "s/^bind-address\s*=.*/bind-address = 10.0.3.139/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql
+
+sudo mysql -u root <<EOF
+CREATE DATABASE db_wordpress;
+CREATE USER 'Josein'@'10.0.3.%' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON db_wordpress.* TO 'Josein'@'10.0.3.%';
+FLUSH PRIVILEGES;
+EOF
