8ae61995dbf549740d72cf24b982247cc4131acb
Hassuna2003 <jmmoujirh01@iesalbarregas.es>
2024-12-05 13:30:27 +0100

    Update nfs.sh

--- Scripts_aprov/nfs.sh
+++ Scripts_aprov/nfs.sh
@@ -4,3 +4,19 @@ sudo apt update -y
 # Instalamos el servidor NFS
 sudo apt install nfs-kernel-server -y
 
+
+sudo apt install nfs-kernel-server -y
+sudo apt install unzip -y
+sudo apt install curl -y
+sudo apt install php php-mysql -y
+sudo apt install mysql-client -y
+
+sudo mkdir -p /var/nfs/compartir
+
+sudo chown nobody:nogroup /var/nfs/compartir 
+
+sudo sed -i '$a /var/nfs/compartir    10.0.2.0/24(rw,sync,no_subtree_check)' /etc/exports
+
+sudo curl -O https://wordpress.org/latest.zip
+
+sudo unzip -o latest.zip -d /var/nfs/compartir/
